8f024ed7e63cfb11ea338a0422535e2e0979aa59
nurmarief <ariefmuhammad897@gmail.com>
2023-10-23 15:54:07 +0700

    create images dir

---
+++ src/images/index.js
@@ -0,0 +1,7 @@
+import './bg-pattern-quotation.svg';
+import './favicon-32x32.png';
+import './image-daniel.jpg';
+import './image-jeanette.jpg';
+import './image-jonathan.jpg';
+import './image-kira.jpg';
+import './image-patrick.jpg';
